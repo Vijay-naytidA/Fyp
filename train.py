1f0a8aab59c63c3a6f2a3bb4abb70aa35e574f58
UltralyticsAssistant <web@ultralytics.com>
2026-03-05 04:12:30 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,27 +1,26 @@
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 from ultralytics import YOLO
-if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/11/yolo11-ACmix-CCFM.yaml') 
-    
+
+if __name__ == "__main__":
+    model = YOLO("ultralytics/cfg/models/11/yolo11-ACmix-CCFM.yaml")
+
     optimizer_params = {
-       # 'lr0': 0.007,  
-       # 'momentum': 0.937,
-      
+        # 'lr0': 0.007,
+        # 'momentum': 0.937,
         # 'weight_decay': 0.0005,
     }
-    
-    
+
     results = model.train(
-        data='/content/Fyp_dataset/hrsid.yaml',
+        data="/content/Fyp_dataset/hrsid.yaml",
         # data='../dataset/HRSID/hrsid.yaml',
         # close_mosaic=0,
         epochs=150,
         imgsz=640,
-        device=0,  
-        optimizer='SGD',
+        device=0,
+        optimizer="SGD",
         batch=16,
-        
-        project='runs/ACyolo',
-        amp=False,  
+        project="runs/ACyolo",
+        amp=False,
     )
